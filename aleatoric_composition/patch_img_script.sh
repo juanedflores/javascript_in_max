7c1ddb46b9a910dbe84f1656e3f703e96110a9c9
Juan Eduardo <juanedflores@gmail.com>
2022-03-29 16:43:15 -0500

    fixed image name

--- aleatoric_composition/patch_img_script.sh
+++ aleatoric_composition/patch_img_script.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 # 1.) Fullscreen Max
-# 2.) Take a sreenshot (Cmd + Shift + 3)
-# 3.) Name it (ex aleatoric_composition1.png)
-# 4.) Run this script ./patch_img_script.sh [filename] (Make sure it is executable)
+# 2.) Align the patch to be one grid unit away from top left corner.
+# 3.) Take a sreenshot (Cmd + Shift + 3)
+# 4.) Name it (ex aleatoric_composition1.png)
+# 5.) Run this script ./patch_img_script.sh [filename] (Make sure it is executable)
 
 fullfilename=$1
 filename=$(basename "$fullfilename")
